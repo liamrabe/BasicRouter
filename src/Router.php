06314f705c2e999a5ed17b0053261f8e6d591803
Liam <liamrabe@hotmail.com>
2022-09-17 16:24:03 +0200

    Version 2.1.1

--- src/Router.php
+++ src/Router.php
@@ -10,7 +10,7 @@ use RuntimeException;
 
 class Router {
 
-	protected const VERSION = '2.1.0';
+	protected const VERSION = '2.1.1';
 
 	protected static array $controller;
 	protected static array $middleware;
